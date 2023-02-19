e6d05567b6ecef6cc320c95d2c3b1c38897c792b
Saf <120998128+safran2k@users.noreply.github.com>
2023-02-19 23:02:31 +0000

    add hover effect to buttons and functionality to add text to calculator screen when button is clicked

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calculator
 Calculator project, combination of HTML, CSS and JavaScript, will be fully functional and interactive
+
+[**Live preview**](https://safran2k.github.io/calculator/index.html)
--- javascript.js
+++ javascript.js
@@ -1,3 +1,33 @@
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
+const bigText = document.querySelector('#big-text');
+const smallText = document.querySelector('#small-text');
+let appendingNumber = true;
+
+numbers.forEach(number => {
+    number.addEventListener('click', ()=> {
+        if(!appendingNumber){
+            bigText.textContent += "  ";
+        }
+        bigText.textContent += number.id;
+        appendingNumber = true;
+    });
+});
+
+operations.forEach(operation => {
+    operation.addEventListener('click', ()=> {
+        if(appendingNumber){
+            bigText.textContent += "  " + operation.textContent;
+        }
+        appendingNumber = false;
+    });
+});
+
+// console.table(Array.from(numbers));
+
+
+
+
 function add(num1, num2){
     return num1 + num2;
 }

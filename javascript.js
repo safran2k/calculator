00a7e85e4151bbe5f28ad4fb851c4047043f6158
Saf <120998128+safran2k@users.noreply.github.com>
2023-02-19 21:26:54 +0000

    initial commit
    create index.html, style.css and javascript.js
    add basic body and buttons of calculator
    add css styling grey background for calculator body buttons and screen
    create basic binary operation functions with no added functionality to calculator itself

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function mult(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operation, num1, num2) {
+    return operation(num1, num2);
+}
